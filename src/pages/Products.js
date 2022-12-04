2563ab1a2e7956e6d9561bfe779358a5754cb3c9
unknown <fredericreact@gmail.com>
2022-12-04 16:46:15 +0800

    Adding Dynamic Routes with Params

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,5 +1,15 @@
 const Products = () => {
-    return <h1>The Products Page</h1>
+    return (
+    <section>
+    <h1>The Products Page</h1>
+    <ul>
+        <li>A book</li>
+        <li>A Carpet</li>
+        <li>An Online Course</li>
+    </ul>
+
+    </section>
+    )
 }
 
 export default Products
